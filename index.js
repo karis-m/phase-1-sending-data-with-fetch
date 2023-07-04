d12c70d870c2f786b0f022eca33d3eaf1f30c38c
moses <moses.kamau@student.moringaschool.com>
2023-07-04 22:40:59 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+function submitData(name , email){
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+            Accept: 'application/json',
+            
+        },
+        body:JSON.stringify({
+            name: 'Steve',
+            email: 'steve@steve.com',
+        })
+    })
+    .then(response => response.json())
+    .then(users => console.log(users))
+    .catch(error => {
+        alert("Bad things! Ragnarők!");
+        console.log(error)
+    })
+}
+submitData('Sam','sam@sam.com')
